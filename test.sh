3eac18202e080ff816d4d8568ccc73bae7322b24
Clemens Tiedt <clemens.tiedt@student.hpi.de>
2020-07-16 00:59:07 +0200

    add exit status to tests

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+TESTS_NUMBER=0
+TESTS_PASSED=0
+
 function unit () {
+  TESTS_NUMBER=$((TESTS_NUMBER+1))
   bash -c "$2"
   if [[ $? -ne 0 ]]; then
     echo $1 " [failed]"
   else
     echo $1 " [ok]"
+    TESTS_PASSED=$((TESTS_PASSED+1))
   fi
 }
 
@@ -21,16 +26,20 @@ unit "Mount..." "sudo mount -t rsramfs none test"
 
 unit "Create File..." "echo 'Hello World' > test/file"
 
+TESTS_NUMBER=$((TESTS_NUMBER+1))
 if [[ `cat test/file` = "Hello World" ]]; then
   echo "File contents... [ok]"
+  TESTS_PASSED=$((TESTS_PASSED+1))
 else
   echo "File contents... [failed]"
 fi
 
 unit "Create Directory..." "mkdir test/dir"
 
+TESTS_NUMBER=$((TESTS_NUMBER+1))
 if [[ -e test/dir ]]; then
   echo "Directory... [ok]"
+  TESTS_PASSED=$((TESTS_PASSED+1))
 else
   echo "Directory... [failed]"
 fi
@@ -40,3 +49,7 @@ unit "Unmount..." "sudo umount test"
 unit "Remove Module..." "sudo rmmod rsramfs"
 
 rm -rf test
+
+echo "Passed $TESTS_PASSED/$TESTS_NUMBER tests"
+
+exit $((TESTS_NUMBER-TESTS_PASSED))
