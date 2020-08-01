48c9776ece3d18d46e0f98bb213b1afd7033b73e
Clemens Tiedt <clemens.tiedt@student.hpi.de>
2020-08-01 23:16:26 +0200

    Remove dependency on cargo xbuild

--- kbuild.mk
+++ kbuild.mk
@@ -3,5 +3,5 @@ src := $(BASE_DIR)/src
 rsramfs-objs := inode.o librsramfs.o
 
 $(obj)/librsramfs.o: $(src)/*.rs
-	(cd $(BASE_DIR); env RUST_TARGET_PATH=$(BASE_DIR) cargo xbuild --target $(TARGET))
+	(cd $(BASE_DIR); env RUST_TARGET_PATH=$(BASE_DIR) cargo build -Z build-std=core,alloc --target $(TARGET))
 	cp $(BASE_DIR)/target/$(TARGET)/debug/librsramfs.a $(obj)/librsramfs.o
