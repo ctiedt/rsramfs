97dab80d41181b9df1b53ab9ccbb374f9d0c08cb
Clemens Tiedt <clemens.tiedt@student.hpi.de>
2020-08-02 14:04:52 +0200

    update acknowledgements

--- src/inode.c
+++ src/inode.c
@@ -1,21 +1,12 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/fs.h>
-#include <linux/pagemap.h>
-#include <linux/highmem.h>
-#include <linux/time.h>
-#include <linux/string.h>
-#include <linux/backing-dev.h>
-#include <linux/sched.h>
-#include <linux/magic.h>
-#include <linux/slab.h>
-#include <linux/uaccess.h>
 #include "rust_fns.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Linus Torvalds");
 MODULE_AUTHOR("Clemens Tiedt");
+MODULE_AUTHOR("Clara Granzow");
 
 void _mapping_set_unevictable(struct address_space *m)
 {
